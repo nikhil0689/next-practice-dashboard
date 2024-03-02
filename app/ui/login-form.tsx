8fe6e9f06c54d2fad3cb5a67f220058930ff848e
nikhil <nikhil0689@gmail.com>
2024-03-02 00:57:34 -0800

    login made easy

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -32,7 +32,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="user@nextmail.com"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -43,7 +43,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="password"
             >
-              Password
+              123456
             </label>
             <div className="relative">
               <input
